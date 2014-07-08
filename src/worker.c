b372dfaa2059729a54e366b30d2cb533b8bd55ca
Thorsten Ball <mrnugget@gmail.com>
2014-07-08 09:38:50 +0200

    Integrate http_parser in most simple way possible

--- src/worker.c
+++ src/worker.c
@@ -7,11 +7,29 @@
 #include "networking.h"
 #include "ipc.h"
 #include "worker.h"
+#include "http_parser.h"
 
 char *response_ok = "HTTP/1.1 200 OK\r\n"
 "Content-Length: 32\r\n"
 "Connection: close\r\n\r\n"
 "It's a UNIX system! I know this!";
+
+struct connection {
+    int complete;
+    int bufsize;
+    char buffer[BUFSIZE];
+};
+
+int message_complete_cb(http_parser *p)
+{
+    struct connection *c = p->data;
+    c->complete = 1;
+    return 0;
+}
+
+static struct http_parser_settings settings = {
+    .on_message_complete = message_complete_cb
+};
 
 static void err_exit(char *msg)
 {
@@ -19,29 +37,38 @@ static void err_exit(char *msg)
     exit(1);
 }
 
-static void handle_connection(int fd, char *buf, int bufsize)
+static void handle_connection(int fd, http_parser *p)
 {
-    int nread;
+    int nread, nparsed;
     int response_len = strlen(response_ok);
 
-    memset(buf, '\0', bufsize);
+    struct connection *c = malloc(sizeof(struct connection));
+    if (c == NULL) err_exit("Could not allocate connection");
+    c->complete = 0;
+    c->bufsize = BUFSIZE;
+    memset(c->buffer, '\0', c->bufsize);
+
+    p->data = c;
 
     do {
-        if ((nread = recv(fd, buf, bufsize, 0)) < 0) {
+        if ((nread = recv(fd, c->buffer, c->bufsize, 0)) < 0) {
             fprintf(stderr, "handle_connection: recv failed\n");
             break;
         }
 
-        if (buf[nread-2] == '\r' && buf[nread-1] == '\n') {
-            // Received a CRLF. Hacky, but works for responding with 200 OK.
+        nparsed = http_parser_execute(p, &settings, c->buffer, nread);
+        if (nparsed != nread) {
+            fprintf(stderr, "nparsed != nread\n");
             break;
         }
+        if (c->complete) break;
     } while (nread > 0);
 
     if (send(fd, response_ok, response_len, 0) != response_len) {
         fprintf(stderr, "handle_connection: send failed\n");
     }
 
+    free(c);
     close(fd);
     return;
 }
@@ -49,14 +76,18 @@ static void handle_connection(int fd, char *buf, int bufsize)
 void worker_loop(int ipc_sock)
 {
     int recvd_conn_fd;
-    char buffer[BUFSIZE];
+
+    http_parser *parser = malloc(sizeof(http_parser));
+    if (parser == NULL) err_exit("Could not allocate parser");
+
+    http_parser_init(parser, HTTP_REQUEST);
 
     for (;;) {
         if (recv_fd(ipc_sock, &recvd_conn_fd) < 0) {
             err_exit("Could not receive recvd_conn_fd");
         }
 
-        handle_connection(recvd_conn_fd, buffer, BUFSIZE);
+        handle_connection(recvd_conn_fd, parser);
 
         if (write(ipc_sock, "", 1) != 1) {
             err_exit("Could write available-signal to socket");
